97db36356f057d0bb1ac86cdec92e91e429c8a94
Itamar-Farias <81915356+Itamar-Farias@users.noreply.github.com>
2021-04-08 09:36:21 -0300

    Update gas.py

--- unidade1/consumo/gas.py
+++ unidade1/consumo/gas.py
@@ -1,8 +1,11 @@
 print '$ python joao.py' 
+
 pos_inicial = float(raw_input())
 litros_inicial = float(raw_input())
 pos_final = float(raw_input())
 litros_final = float(raw_input())
+
 dist = pos_final - pos_inicial
 delta_consumo = litros_inicial - litros_final
+
 print 'O consumo total foi de %.1f Km/l.' % (dist / delta_consumo)
